4e941e9aebdfef3995a392ed7b5b84e664e56b99
Sushan Paudyal <sushan.paudyal@gmail.com>
2019-01-13 01:29:30 +0545

    Fixing Bugs in Paginations Loops

--- shop.php
+++ shop.php
@@ -145,62 +145,64 @@ include ("functions/functions.php");
                 <?php
                 if(!isset($_GET['p_cat'])){
                     if(!isset($_GET['cat'])){
-                        $per_page = 6;
-                        if(isset($_GET['page'])){
-                            $page = $_GET['page'];
-                        } else {
-                            $page = 1;
-                        }
-                        $start_from = ($page-1) * $per_page;
-                        $get_products = "SELECT * FROM products order by 1 DESC LIMIT $start_from, $per_page";
-                        $run_products = mysqli_query($con, $get_products);
-                        while($row_products = mysqli_fetch_array($run_products)){
-                            $pro_id = $row_products['product_id'];
-                            $pro_title = $row_products['product_title'];
-                            $pro_price = $row_products['product_price'];
-                            $pro_img1 = $row_products['product_img1'];
-                        ?>
-                <div class="col-md-4 col-sm-6 center-responsive">
-                    <div class="product">
-                        <a href="details.php">
-                            <img src="admin_area/product_images/<?php echo $pro_img1; ?>" alt="" class="img-responsive">
-                        </a>
-                        <div class="text">
-                            <h3>
-                                <a href="details.php?pro_id=<?php echo $pro_id; ?>"><?php echo $pro_title; ?></a>
-                            </h3>
-                            <p class="price">Rs. <?php echo $pro_price; ?></p>
-                            <p class="buttons">
-                                <a href="details.php?pro_id=<?php echo $pro_id; ?>" class="btn btn-default">View Details</a>
-                                <a href="details.php?pro_id=<?php echo $pro_id; ?>" class="btn btn-primary">
-                                    <i class="fa fa-shopping-cart"></i> Add to Cart
-                                </a>
-                            </p>
+                $per_page = 6;
+                if (isset($_GET['page'])) {
+                    $page = $_GET['page'];
+                } else {
+                    $page = 1;
+                }
+                $start_from = ($page - 1) * $per_page;
+                $get_products = "SELECT * FROM products order by 1 DESC LIMIT $start_from, $per_page";
+                $run_products = mysqli_query($con, $get_products);
+                while ($row_products = mysqli_fetch_array($run_products)) {
+                    $pro_id = $row_products['product_id'];
+                    $pro_title = $row_products['product_title'];
+                    $pro_price = $row_products['product_price'];
+                    $pro_img1 = $row_products['product_img1'];
+                    ?>
+                    <div class="col-md-4 col-sm-6 center-responsive">
+                        <div class="product">
+                            <a href="details.php">
+                                <img src="admin_area/product_images/<?php echo $pro_img1; ?>" alt=""
+                                     class="img-responsive">
+                            </a>
+                            <div class="text">
+                                <h3>
+                                    <a href="details.php?pro_id=<?php echo $pro_id; ?>"><?php echo $pro_title; ?></a>
+                                </h3>
+                                <p class="price">Rs. <?php echo $pro_price; ?></p>
+                                <p class="buttons">
+                                    <a href="details.php?pro_id=<?php echo $pro_id; ?>" class="btn btn-default">View
+                                        Details</a>
+                                    <a href="details.php?pro_id=<?php echo $pro_id; ?>" class="btn btn-primary">
+                                        <i class="fa fa-shopping-cart"></i> Add to Cart
+                                    </a>
+                                </p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <?php } } } ?>
+                <?php } ?>
             </div>
 
             <center>
                 <ul class="pagination">
                     <?php
-                        $query = "SELECT * FROM products";
-                        $result = mysqli_query($con, $query);
-                        $total_records = mysqli_num_rows($result);
-                        $total_pages = ceil($total_records / $per_page);
-                        echo "
-                           <li> <a href='shop.php?page=1'> ".'First Page'." </a> </li>
+                    $query = "SELECT * FROM products";
+                    $result = mysqli_query($con, $query);
+                    $total_records = mysqli_num_rows($result);
+                    $total_pages = ceil($total_records / $per_page);
+                    echo "
+                           <li> <a href='shop.php?page=1'> " . 'First Page' . " </a> </li>
                         ";
-                        for($i= 1; $i <= $total_pages; $i++){
-                            echo "
-                            <li> <a href='shop.php?page=".$i."'> ".$i." </a> </li>
+                    for ($i = 1; $i <= $total_pages; $i++) {
+                        echo "
+                            <li> <a href='shop.php?page=" . $i . "'> " . $i . " </a> </li>
                             ";
-                        }
+                    }
 
-                        echo "<li> <a href='shop.php?page=$total_pages'>".'Last Page'."</a> </li>";
+                    echo "<li> <a href='shop.php?page=$total_pages'>" . 'Last Page' . "</a> </li>";
 
-                    ?>
+                    }    }  ?>
                 </ul>
             </center>
         </div>
